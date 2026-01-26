f09d32280452f21dd5e4dbabb70a19e992444582
Nils Mohr <mail@nils-finke.com>
2026-01-26 07:49:31 +0000

    Fix coloring in sheet

--- scripts/update_submission_gsheet.py
+++ scripts/update_submission_gsheet.py
@@ -222,28 +222,20 @@ for idx, col in enumerate(subs_df.columns):
 
     format_cell_range(worksheet, col_id, fmt)
 
-# NOTE: The google sheets API has changed and now needs RBG instead of RGB color definitions. While I am creating a PR in the supporting webcolor package to fix the problem for pytanis, I am using the following workaround.
-from webcolors import name_to_rgb
-
+# Mark rows based on their state
 mask = (subs_df["State"] == 'rejected') | (subs_df["State"] == 'withdrawn') | (subs_df["State"] == 'canceled')
-firebrick_rgb_color = name_to_rgb('firebrick')
-mark_rows(worksheet, mask, [firebrick_rgb_color.red / 255, firebrick_rgb_color.green / 255, firebrick_rgb_color.blue / 255])
+mark_rows(worksheet, mask, 'firebrick')
 
 mask = (subs_df["State"] == 'confirmed')
-green_rgb_color = name_to_rgb('green')
-mark_rows(worksheet, mask, [green_rgb_color.red / 255, green_rgb_color.green / 255, green_rgb_color.blue / 255])
+mark_rows(worksheet, mask, 'green')
 
 mask = (subs_df["State"] == 'accepted')
-limegreen_rgb_color = name_to_rgb('limegreen')
-mark_rows(worksheet, mask, [limegreen_rgb_color.red / 255, limegreen_rgb_color.green / 255, limegreen_rgb_color.blue / 255])
+mark_rows(worksheet, mask, 'limegreen')
 
 mask = (subs_df["Pending state"] == 'rejected')
-lightcoral_rgb_color = name_to_rgb('lightcoral')
-mark_rows(worksheet, mask, [lightcoral_rgb_color.red / 255, lightcoral_rgb_color.green / 255, lightcoral_rgb_color.blue / 255])
+mark_rows(worksheet, mask, 'lightcoral')
 
 mask = (subs_df["Pending state"] == 'accepted')
-greenyellow_rgb_color = name_to_rgb('greenyellow')
-mark_rows(worksheet, mask, [greenyellow_rgb_color.red / 255, greenyellow_rgb_color.green / 255, greenyellow_rgb_color.blue / 255])
-
+mark_rows(worksheet, mask, 'greenyellow')
 
 print('Updated', len(subs_df), 'submissions.')
